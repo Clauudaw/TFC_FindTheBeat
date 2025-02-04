cad53605482085dc265e727a8c8eef8fe4a411f5
Clauudaw <Claudiardaw@gmail.com>
2025-02-04 13:31:04 +0100

    ultimos cmabios

--- reservas.php
+++ reservas.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['user_id'])) {
 $user_id = $_SESSION['user_id'];
 
 // Conectar a la base de datos
-include 'db.php';
+include './db.php';
 
 // Obtener el ID del espacio desde la URL
 $space_id = isset($_GET['space_id']) ? (int)$_GET['space_id'] : 0;
@@ -70,6 +70,7 @@ $space = $stmt->fetch(PDO::FETCH_ASSOC);
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -80,114 +81,131 @@ $space = $stmt->fetch(PDO::FETCH_ASSOC);
         body {
             background-color: #f8f9fa;
         }
+
         .card {
             border-radius: 10px;
         }
+
         .btn-primary {
             background-color: #3D8168;
             border-color: #3D8168;
         }
+
         .btn-primary:hover {
             background-color: #316f53;
         }
-        .form-floating > label {
+
+        .form-floating>label {
             color: #6c757d;
         }
     </style>
 </head>
+
 <body>
 
-<?php include_once './components/header.php'; ?>
+    <?php include_once './components/header.php'; ?>
 
-<div class="container py-5">
-<div class="row justify-content-center">
-<div class="col-lg-10">
-<div class="row">
-    <!-- caja de la izquierda-->
-    <div class="col-md-4">
-                    <div class="card shadow">
-                        <img src="<?php echo $space['imagen']; ?>" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo htmlspecialchars($space['titulo']); ?></h5>
-                            <p class="card-text"><?php echo htmlspecialchars($space['direccion']); ?></p>
-                            <p class="card-text"><?php echo substr(htmlspecialchars($space['descripcion']), 0, 100) . '...'; ?></p>
-                            <p class="fw-bold">Precio: <?php echo number_format($space['precio'], 2); ?>€/hora</p>
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-10">
+                <div class="row">
+                    <!-- Caja de la izquierda -->
+                    <div class="col-md-4">
+                        <div class="card shadow">
+                            <img src="<?php echo $space['imagen']; ?>" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo htmlspecialchars($space['titulo']); ?></h5>
+                                <p class="card-text"><?php echo htmlspecialchars($space['direccion']); ?></p>
+                                <p class="card-text"><?php echo substr(htmlspecialchars($space['descripcion']), 0, 100) . '...'; ?></p>
+                                <p class="fw-bold">Precio: <?php echo number_format($space['precio'], 2); ?>€/hora</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Formulario de reserva -->
+                    <div class="col-md-8">
+                        <div class="card shadow p-4">
+                            <h1 class="text-center mb-4">Reservar este Espacio</h1>
+
+                            <form id="reservationForm" method="POST">
+                                <div class="row">
+                                    <div class="col-md-6 mb-3 form-floating">
+                                        <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Ingresa tu nombre">
+                                        <label for="nombre">Nombre</label>
+                                        <span id="nombreError" class="text-danger"></span>
+                                    </div>
+                                    <div class="col-md-6 mb-3 form-floating">
+                                        <input type="text" class="form-control" id="apellidos" name="apellidos" placeholder="Ingresa tus apellidos">
+                                        <label for="apellidos">Apellidos</label>
+                                        <span id="apellidosError" class="text-danger"></span>
+                                    </div>
+                                    <div class="col-md-6 mb-3 form-floating">
+                                        <input type="text" class="form-control" id="dni" name="dni" placeholder="Ingresa tu DNI">
+                                        <label for="dni">DNI</label>
+                                        <span id="dniError" class="text-danger"></span>
+                                    </div>
+                                    <div class="col-md-6 mb-3 form-floating">
+                                        <input type="text" class="form-control" id="correo" name="correo" placeholder="Ingresa tu correo electrónico">
+                                        <label for="correo">Correo Electrónico</label>
+                                        <span id="correoError" class="text-danger"></span>
+                                    </div>
+                                    <div class="col-md-6 mb-3 form-floating">
+                                        <input type="date" class="form-control" id="fecha_nacimiento" name="fecha_nacimiento" placeholder="Ingresa tu fecha_nacimiento" required>
+                                        <label for="fecha_nacimiento">Fecha de nacimiento</label>
+                                        <span id="fecha_nacimientoError" class="text-danger"></span>
+                                    </div>
+
+                                    <div class="col-md-6 mb-3 form-floating">
+                                        <input type="text" class="form-control" id="telefono" name="telefono" placeholder="Ingresa tu número de teléfono">
+                                        <label for="telefono">Teléfono</label>
+                                        <span id="telefonoError" class="text-danger"></span>
+                                    </div>
+                                </div>
+
+                                <div class="mb-3 form-floating">
+                                    <input type="date" class="form-control" id="fecha_reserva" name="fecha_reserva">
+                                    <label for="fecha_reserva">Fecha de Reserva</label>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6 mb-3 form-floating">
+                                        <input type="time" class="form-control" id="hora_inicio" name="hora_inicio">
+                                        <label for="hora_inicio">Hora de Inicio</label>
+                                    </div>
+                                    <div class="col-md-6 mb-3 form-floating">
+                                        <input type="time" class="form-control" id="hora_fin" name="hora_fin">
+                                        <label for="hora_fin">Hora de Fin</label>
+                                    </div>
+                                </div>
+
+                                <div class="mb-3 form-floating">
+                                    <select class="form-control" id="metodo_pago" name="metodo_pago">
+                                        <option value="" selected disabled>Selecciona una opción</option>
+                                        <option value="tarjeta">Tarjeta</option>
+                                        <option value="paypal">PayPal</option>
+                                        <option value="Bizum">Bizum</option>
+                                    </select>
+                                    <label for="metodo_pago">Método de Pago</label>
+                                </div>
+                                <button type="submit" class="btn btn-primary w-100">Confirmar Reserva</button>
+                            </form>
+
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer"></div>
 
-<div class="col-md-8">
-<div class="card shadow p-4">
-    <h1 class="mb-4">Reservar este Espacio</h1>
-    <form method="POST" class="row g-3 needs-validation" novalidate>
-        <input type="hidden" name="space_id" value="<?php echo $space_id; ?>">
-
-        <div class="col-md-6 form-floating">
-            <input type="text" class="form-control" name="nombre" required>
-            <label class="form-label">Nombre</label>       
-        </div>
-        <div class="col-md-6 form-floating">
-            <input type="text" class="form-control" name="apellidos" required>
-            <label class="form-label">Apellidos</label>        
-        </div>
-        <div class="col-md-6 form-floating">
-            <input type="text" class="form-control" name="dni" required>
-            <label class="form-label">DNI</label>
-        </div>
-        <div class="col-md-6 form-floating">
-            <input type="email" class="form-control" name="correo" required>
-            <label class="form-label">Correo Electrónico</label>
-        </div>
-        <div class="col-md-6 form-floating">
-            <input type="date" class="form-control" name="fecha_nacimiento" required>
-            <label class="form-label">Fecha de Nacimiento</label>
-        </div>
-        <div class="col-md-6 form-floating">
-            <input type="tel" class="form-control" name="telefono" required>
-            <label class="form-label">Teléfono</label>
-        </div>
-        <div class="col-md-4 form-floating">
-            <select class="form-select" name="metodo_pago" required>
-                <option value="" selected disabled>Seleccione una opcion</option>
-                <option value="tarjeta">Tarjeta de Crédito</option>
-                <option value="paypal">PayPal</option>
-                <option value="bizum">Transferencia Bancaria</option>
-            </select>
-            <label class="form-label">Método de Pago</label>
-        </div>
-        <div class="col-md-6 form-floating">
-            <input type="date" class="form-control" name="fecha_reserva" required>
-            <label class="form-label">Fecha de Reserva</label>
-        </div>
-        <div class="col-md-3 form-floating">
-            <input type="time" class="form-control" name="hora_inicio" required>
-            <label class="form-label">Hora de Inicio</label>
-        </div>
-        <div class="col-md-3 form-floating">
-            <input type="time" class="form-control" name="hora_fin" required>
-            <label class="form-label">Hora de Fin</label>
-        </div>
-        <div class="col-12 w-100">
-        <button type="submit" class="btn btn-primary">Confirmar Reserva</button>
-        </div>
-    </form>
-    </div>
-    </div>
-    </div>
-</div>
-    </div>
-    
-    <!-- Modal de Confirmación -->
-    <div class="modal fade" id="reservaConfirmadaModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+    <!-- Modal de confirmación -->
+    <div class="modal fade" id="thankYouModal" tabindex="-1" aria-labelledby="thankYouModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modalLabel">Reserva Confirmada</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+                    <h5 class="modal-title" id="thankYouModalLabel">¡Reserva Confirmada!</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ✅ ¡Tu reserva ha sido confirmada con éxito!  
-                    Puedes verla en tu <a href="user_dashboard.php">panel personal</a>.
+                    Tu reserva ha sido realizada exitosamente. Puedes verla en tu panel personal.
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-success" data-bs-dismiss="modal">Aceptar</button>
@@ -196,24 +214,117 @@ $space = $stmt->fetch(PDO::FETCH_ASSOC);
         </div>
     </div>
 
-</div>
-<div id="footer"></div>
-<script>
-    function mostrarModalReserva() {
-        var modal = new bootstrap.Modal(document.getElementById('reservaConfirmadaModal'));
-        modal.show();
-    }
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/app.js"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const form = document.getElementById('reservationForm');
+            const thankYouModal = new bootstrap.Modal(document.getElementById('thankYouModal'));
 
-    document.addEventListener("DOMContentLoaded", function () {
-    // Verificar si la variable de sesión está presente (se puede pasar desde PHP)
-    <?php if (isset($_SESSION['reservation_success']) && $_SESSION['reservation_success'] == true): ?>
-        mostrarModalReserva(); // Muestra el modal si la variable está definida
-        <?php unset($_SESSION['reservation_success']); // Eliminar la variable de sesión después de mostrar el modal ?>
-    <?php endif; ?>
-});
-</script>
+            const nameRegex = /^[a-zA-Z\s]+$/;
+            const dniRegex = /^[0-9]{8}[A-Za-z]$/;
+            const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+            const phoneRegex = /^[0-9]{9}$/;
 
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<script src="/assets/js/app.js"></script>
+            function validateForm() {
+                let isValid = true;
+
+                const nombre = document.getElementById('nombre');
+                const nombreError = document.getElementById('nombreError');
+                if (!nombre.value.match(nameRegex)) {
+                    nombreError.textContent = "El nombre solo puede contener letras y espacios.";
+                    isValid = false;
+                } else {
+                    nombreError.textContent = "";
+                }
+
+                const apellidos = document.getElementById('apellidos');
+                const apellidosError = document.getElementById('apellidosError');
+                if (!apellidos.value.match(nameRegex)) {
+                    apellidosError.textContent = "Los apellidos solo pueden contener letras y espacios.";
+                    isValid = false;
+                } else {
+                    apellidosError.textContent = "";
+                }
+
+                const dni = document.getElementById('dni');
+                const dniError = document.getElementById('dniError');
+                if (!dni.value.match(dniRegex)) {
+                    dniError.textContent = "Introduce un DNI válido.";
+                    isValid = false;
+                } else {
+                    dniError.textContent = "";
+                }
+
+                const correo = document.getElementById('correo');
+                const correoError = document.getElementById('correoError');
+                if (!correo.value.match(emailRegex)) {
+                    correoError.textContent = "Introduce un correo electrónico válido.";
+                    isValid = false;
+                } else {
+                    correoError.textContent = "";
+                }
+
+                const telefono = document.getElementById('telefono');
+                const telefonoError = document.getElementById('telefonoError');
+                if (!telefono.value.match(phoneRegex)) {
+                    telefonoError.textContent = "Introduce un número de teléfono válido.";
+                    isValid = false;
+                } else {
+                    telefonoError.textContent = "";
+                }
+
+                const fecha_nacimiento = document.getElementById('fecha_nacimiento');
+                const fecha_nacimientoError = document.getElementById('fecha_nacimientoError');
+
+                function calcularEdad(fechaNacimiento) {
+                    const hoy = new Date();
+                    const nacimiento = new Date(fechaNacimiento);
+                    let edad = hoy.getFullYear() - nacimiento.getFullYear();
+                    const mes = hoy.getMonth();
+                    const dia = hoy.getDate();
+
+                    // Verificar si aún no ha cumplido años este año
+                    if (mes < nacimiento.getMonth() || (mes === nacimiento.getMonth() && dia < nacimiento.getDate())) {
+                        edad--;
+                    }
+
+                    return edad;
+                }
+
+                const edad = calcularEdad(fecha_nacimiento.value);
+
+                if (!fecha_nacimiento.value || isNaN(Date.parse(fecha_nacimiento.value))) {
+                    fecha_nacimientoError.textContent = "La fecha de nacimiento es obligatoria.";
+                    isValid = false;
+                } else if (edad < 18) {
+                    fecha_nacimientoError.textContent = "Debes tener al menos 18 años.";
+                    isValid = false;
+                } else {
+                    fecha_nacimientoError.textContent = ""; // Limpiar mensaje de error si la edad es válida
+                }
+
+
+                return isValid;
+            }
+
+            form.addEventListener('submit', function(event) {
+                event.preventDefault();
+
+                if (validateForm()) {
+                    // Aquí puedes enviar los datos al servidor si es necesario
+                    thankYouModal.show();
+
+                    // Agregar el evento de clic en el botón "Aceptar"
+                    document.querySelector(".btn-success[data-bs-dismiss='modal']").addEventListener("click", function() {
+                        form.submit(); // Enviar el formulario cuando el usuario haga clic en "Aceptar"
+                    });
+                }
+            });
+        });
+    </script>
+
+
 </body>
+
 </html>
