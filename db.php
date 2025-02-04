d96583357cc8ed4f90d657281b24fbbe0982a545
Clauudaw <Claudiardaw@gmail.com>
2025-02-04 14:46:52 +0100

    cmabio db

--- db.php
+++ db.php
@@ -1,12 +1,14 @@
 <?php
-$host = 'localhost';
-$dbname = 'findthebeat';
-$username = 'root';
+  $host_name = 'db5017147270.hosting-data.io';
+  $database = 'dbs13781720';
+  $user_name = 'dbu682517';
+  $password = 'Edgar.allan.poe.1704';
+  $dbh = null;
 
-try {
-    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
-}
+  try {
+    $dbh = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password);
+  } catch (PDOException $e) {
+    echo "Error!:" . $e->getMessage() . "<br/>";
+    die();
+  }
 ?>
