08e37fa0ab7a2412ab3b4d5e7273ef9a51e14bf2
Clauudaw <Claudiardaw@gmail.com>
2025-02-04 17:08:36 +0100

    db

--- db.php
+++ db.php
@@ -1,4 +1,17 @@
 <?php
+// Configuración de la base de datos
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_NAME", "findthebeat");
+
+try {
+    $db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error al conectar con la base de datos: " . $e->getMessage());
+}
+/*
+<?php
   $host_name = 'db5017147270.hosting-data.io';
   $database = 'dbs13781720';
   $user_name = 'dbu682517';
@@ -11,4 +24,6 @@
     echo "Error!:" . $e->getMessage() . "<br/>";
     die();
   }
+?>*/
 ?>
+
